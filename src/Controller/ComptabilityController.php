ed3d3f32aad41c747d68f6fcb94cfa0b385cfbcc
Justin Onomo <justinonomo@gmail.com>
2022-04-04 13:12:09 +0200

    create order route and find a way to set a link to active class

--- src/Controller/ComptabilityController.php
+++ src/Controller/ComptabilityController.php
@@ -6,13 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/comptability")
+ */
 class ComptabilityController extends AbstractController
 {
     /**
-     * @Route("/comptability", name="app_comptability")
+     * @Route("/", name="app_comptability")
      */
     public function index(): Response
     {
         return $this->render('comptability/index.html.twig');
+    }
+
+    /**
+     * @Route("/order", name="app_comptability_order")
+     */
+    public function order(): Response
+    {
+        return $this->render('comptability/order.html.twig');
     }
 }
